44c002469516004737ad9c0ac4460deb0cd922fe
Carla Souza <carlavanessasp275@gmail.com>
2026-03-01 22:47:23 -0300

    apaguei arquivos de outros projetos

--- Aula 7 projeto/Aula 7 projeto/ClubeCaissa/script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('titulo').style.animationPlayState = 'running';
-    document.getElementById('subtitulo').style.animationPlayState = 'running';
-  });
-  
