21362f9c0abfb0b06f5b7bf9e5b5b9a1ae7d11f3
Carla Souza <carlavanessasp275@gmail.com>
2026-03-01 22:42:24 -0300

    Primeiro commit

---
+++ Aula 7 projeto/Aula 7 projeto/ClubeCaissa/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('titulo').style.animationPlayState = 'running';
+    document.getElementById('subtitulo').style.animationPlayState = 'running';
+  });
+  
